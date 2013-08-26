d22ad582d66b4ab688662549db9502a1d9dd4800
jakeols <jake_ols@live.com>
2013-08-26 18:31:58 -0400

    Revert "logout"
    This reverts commit e45ce2ea3508b3568d4fdc672e33408e8db2ecb7.

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -9,7 +9,6 @@
       <li><a href="<?php echo base_url()?>index.php/User/view_inventory">View Inventory</a></li>
       <li><a href="<?php echo base_url()?>index.php/User/view_new_transaction">New Transaction</a></li>
       <li><a href="<?php echo base_url()?>index.php/User/view_transactions">View Transactions</a></li>
-      <li><a href="<?php echo base_url()?>index.php/auth/logout">Logout</a></li>
     </ul>
   </div>
 </div>
